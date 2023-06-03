0b76a2c7efd12f4f44b892fc303b2dae61ab09b8
noortechilab <juttnoor756@gmail.com>
2023-06-03 14:51:37 +0500

    init

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const Services = () => {
   return (
-    <div className="text-white " style={{ height: "100vh" ,background:"#02050e"}}>
-      Services
-    </div>
-  );
-};
+    <div className='text-white' style={{background:"#000",height:"100vh"}}>Services</div>
+  )
+}
 
-export default Services;
+export default Services

0f874001755793a30953632348dd23a5a7c63bc2
noortechilab <juttnoor756@gmail.com>
2023-06-09 00:15:13 +0500

    init

--- src/components/homePageCom/Aine.js
+++ src/components/homePageCom/Aine.js
@@ -6,47 +6,56 @@ import { AiFillStar } from "react-icons/ai";
 const Aine = () => {
   return (
     <>
-      <div className="bg-aine-sec">
+      <div className="bg-aine-sec py-5">
         <div className="container">
           <div className="row ">
-            <div className="col-lg-6 text-white ">
-              <div className="fs-1 fw-bold ">
+            <div className="col-lg-6 text-white">
+              <div className="fs-1 fw-bold hello-aine">
                 You do the business, <br /> we'll handle the money
               </div>
-              <div className="text-secondary mt-3 fw-bold">
+              <div className="text-secondary mt-4 fw-bold hello-para">
                 with the right credit card,you can improve your <br />
                 financial life by building credit, earning rewards and <br />{" "}
                 saving money. But with hundreds of credit cards on <br />
                 the market
               </div>
-              <div className="mt-3 mb-5">
-                <Button label={"Get started"}></Button>
+              <div className="mt-4 mb-5 hello-btn">
+                <Button label={"Get started"} className="btn-ok"></Button>
               </div>
             </div>
-            <div className="col-lg-6 text-white d-flex justify-content-end flex-column">
-        
-              
-                  {/* /// */}
-                 <div className="bg-hover px-3 p-2">
-                  <AiFillStar className="fs-1 icon-anie"/><span className="ms-3 fs-3">Rewards</span>
-                  <div className="ms-5 ps-2">The best credit cards after some tantalizing combinations of promotions and prizes</div>
-                 </div>
-                  {/* /// */}
-                     {/* /// */}
-                 <div className="bg-hover px-3 p-2">
-                  <AiFillStar className="fs-1 icon-anie"/><span className="ms-3 fs-3">Rewards</span>
-                  <div className="ms-5 ps-2">The best credit cards after some tantalizing combinations of promotions and prizes</div>
-                 </div>
-                    {/* /// */}
-                    <div className="bg-hover px-3 p-2">
-                  <AiFillStar className="fs-1 icon-anie"/><span className="ms-3 fs-3">Rewards</span>
-                  <div className="ms-5 ps-2">The best credit cards after some tantalizing combinations of promotions and prizes</div>
-                 </div>
+            <div className="col-lg-6 text-white ">
+              {/* /// */}
+              <div className="bg-hover px-3 p-2 ">
+                <AiFillStar className="fs-1 icon-anie" />
+                <span className="ms-3 fs-3">Rewards</span>
+                <div className="ms-5">
+                  The best credit cards after some tantalizing combinations of
+                  promotions and prizes
+                </div>
+              </div>
+              {/* /// */}
+              {/* /// */}
+              <div className="bg-hover px-3 p-2">
+                <AiFillStar className="fs-1 icon-anie" />
+                <span className="ms-3 fs-3">Rewards</span>
+                <div className="ms-5 ">
+                  The best credit cards after some tantalizing combinations of
+                  promotions and prizes
+                </div>
+              </div>
+              {/* /// */}
+              <div className="bg-hover px-3 p-2">
+                <AiFillStar className="fs-1 icon-anie" />
+                <span className="ms-3 fs-3">Rewards</span>
+                <div className="ms-5 ">
+                  The best credit cards after some tantalizing combinations of
+                  promotions and prizes
                 </div>
               </div>
             </div>
           </div>
-      
+        </div>
+      </div>
     </>
   );
 };

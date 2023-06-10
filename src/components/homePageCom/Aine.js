8162a7097e8eb58913e410fd8960ef1d1918a5db
noortechilab <juttnoor756@gmail.com>
2023-06-10 23:02:58 +0500

    init

--- src/components/homePageCom/Aine.js
+++ src/components/homePageCom/Aine.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "../css/Aine.scss";
 import Button from "../button/Button";
-import { AiFillStar } from "react-icons/ai";
-import { MdVerifiedUser } from "react-icons/md";
-import { BsFillSendFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import tech from "../assets/tech.png";
 // import seo from "../assets/seo.png"
 const Aine = () => {
   return (
@@ -14,19 +13,19 @@ const Aine = () => {
             <div className="content-column col-lg-6 col-md-12 col-sm-12 order-2">
               <div className="inner-column">
                 <div className="sec-title">
-                  <span className="title ">About Css3transition</span>
+                  <span className="title ">About Coding master</span>
                   <h2 className="text-white">
                     We are Creative Tech Enthusiast working since 2015
                   </h2>
                 </div>
-                <div className="text">
-                  I am Rahul Yaduvanshi works at Css3 Transition since last 3
-                  years. We are here to provide touch notch solution for your
-                  website or web application that helps you to make your website
-                  look attractive & efficient in handling by creating usefull
-                  plugins thats you need.
+                <div className="text text-white">
+                  Coding master works at Css3 Transition since last 3 years. We
+                  are here to provide touch notch solution for your website or
+                  web application that helps you to make your website look
+                  attractive & efficient in handling by creating usefull plugins
+                  thats you need.
                 </div>
-                <div className="text">
+                <div className="text text-white">
                   We are here to serve you next level tutorial that currently in
                   trend to match you with your expertise. Css3 transition is a
                   learning website. where you can find many good quality content
@@ -36,9 +35,9 @@ const Aine = () => {
                   Photoshop and adobe allustrator.
                 </div>
                 <div className="btn-box">
-                  <a href="#" className="theme-btn btn-style-one">
-                    Contact Us
-                  </a>
+                  <Link>
+                    <Button label={"Explore more"} />
+                  </Link>
                 </div>
               </div>
             </div>
@@ -50,16 +49,9 @@ const Aine = () => {
                         <h2>Rahul Kumar Yadav</h2>
                         <span>Web Developer</span>
                       </div> */}
-                <figure className="image-1">
-                  <a href="#" className="lightbox-image" data-fancybox="images">
-                    <img
-                      title="Rahul Kumar Yadav"
-                      className="img-aine"
-                      src="https://i.ibb.co/QP6Nmpf/image-1-about.jpg"
-                      alt=""
-                    />
-                  </a>
-                </figure>
+                <div className="image-1 ">
+                  <img title="" className="img-aine w-75 mt-3 " src={tech} alt="" />
+                </div>
               </div>
             </div>
           </div>
